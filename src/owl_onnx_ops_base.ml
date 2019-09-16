dbf9c374b1034ae7ca26379c241f2d5e921be9f9
Liang Wang <liang.wang@nokia.com>
2019-09-16 22:39:10 +0300

    coding onnx converter ...

---
+++ src/owl_onnx_ops_base.ml
@@ -0,0 +1,18 @@
+(*
+ * OWL - OCaml Scientific and Engineering Computing
+ * Copyright (c) 2016-2019 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
+
+type t =
+  | Add of Owl_onnx_ops_math.Add.t
+  | Sub of Owl_onnx_ops_math.Sub.t
+  | Mul of Owl_onnx_ops_math.Mul.t
+  | Div of Owl_onnx_ops_math.Div.t
+
+
+let op_type = function
+  | Add _ -> Owl_onnx_ops_math.Add.op_type
+  | Sub _ -> Owl_onnx_ops_math.Sub.op_type
+  | Mul _ -> Owl_onnx_ops_math.Mul.op_type
+  | Div _ -> Owl_onnx_ops_math.Div.op_type
---
+++ src/owl_onnx_ops_generic.ml
@@ -0,0 +1,18 @@
+(*
+ * OWL - OCaml Scientific and Engineering Computing
+ * Copyright (c) 2016-2019 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
+
+module type Sig = sig
+
+
+
+end
+
+
+module Make (M : Sig) = struct
+
+  type t
+
+end
