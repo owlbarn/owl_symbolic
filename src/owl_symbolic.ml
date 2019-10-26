f3a7e220296523ccd3c0d1a5a18f22c81f4c3c47
jzstark <rho.ajax@gmail.com>
2019-10-26 17:00:23 +0100

    renaming...

--- src/owl_symbolic.ml
+++ src/owl_symbolic.ml
@@ -9,9 +9,9 @@
  * using ONNX protobuf. By so doing, we can offload computations defined by
  * Owl to various accelerators.
  *
- * Refer to 
+ * Refer to
  *     1. https://onnx.ai/
  *     2. https://github.com/onnx/onnx/
  *)
 
-module Specs = Owl_onnx_specs
+module Specs = Owl_symbolic_specs
--- src/owl_symbolic_symbol.ml
+++ src/owl_symbolic_symbol.ml
@@ -5,22 +5,22 @@
 
 type t =
   | NOOP
-  | Add of Owl_onnx_ops_math.Add.t
-  | Sub of Owl_onnx_ops_math.Sub.t
-  | Mul of Owl_onnx_ops_math.Mul.t
-  | Div of Owl_onnx_ops_math.Div.t
+  | Add of Owl_symbolic_ops_math.Add.t
+  | Sub of Owl_symbolic_ops_math.Sub.t
+  | Mul of Owl_symbolic_ops_math.Mul.t
+  | Div of Owl_symbolic_ops_math.Div.t
 
 let name = function
-  | Add x -> Owl_onnx_ops_math.Add.(x.name)
-  | Sub x -> Owl_onnx_ops_math.Sub.(x.name)
-  | Mul x -> Owl_onnx_ops_math.Mul.(x.name)
-  | Div x -> Owl_onnx_ops_math.Div.(x.name)
-  | _     -> failwith "owl_onnx_symbol.name"
+  | Add x -> Owl_symbolic_ops_math.Add.(x.name)
+  | Sub x -> Owl_symbolic_ops_math.Sub.(x.name)
+  | Mul x -> Owl_symbolic_ops_math.Mul.(x.name)
+  | Div x -> Owl_symbolic_ops_math.Div.(x.name)
+  | _     -> failwith "owl_symbolic_symbol.name"
 
 
 let op_type = function
-  | Add _ -> Owl_onnx_ops_math.Add.op_type
-  | Sub _ -> Owl_onnx_ops_math.Sub.op_type
-  | Mul _ -> Owl_onnx_ops_math.Mul.op_type
-  | Div _ -> Owl_onnx_ops_math.Div.op_type
+  | Add _ -> Owl_symbolic_ops_math.Add.op_type
+  | Sub _ -> Owl_symbolic_ops_math.Sub.op_type
+  | Mul _ -> Owl_symbolic_ops_math.Mul.op_type
+  | Div _ -> Owl_symbolic_ops_math.Div.op_type
   | _     -> failwith "owl_onnx_symbol.op_type"
