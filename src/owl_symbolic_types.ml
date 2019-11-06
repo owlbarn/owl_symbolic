dcc5b0548fb8295c18d83934de6753cb4bfc6ba4
jzstark <rho.ajax@gmail.com>
2019-11-06 21:25:12 +0000

    coding

--- src/owl_symbolic_types.ml
+++ src/owl_symbolic_types.ml
@@ -9,4 +9,26 @@ type tensor_typ =
   | C
   | Z
 
+(** NOTE: no real data should be included in symbolic computation *)
+type tensor = {
+  dtype          : string;
+  tensor_shape   : int array;
+  string_val     : string array option;
+  float_val      : float array option;
+  int_val        : int array option;
+  tensor_content : bytes option;
+}
+
+type attrvalue =
+  | ATTR_Nil
+  | ATTR_Int      of int
+  | ATTR_Bool     of bool
+  | ATTR_Type     of string
+  | ATTR_Float    of float
+  | ATTR_Shape    of int array
+  | ATTR_String   of string
+  | ATTR_Tensor   of tensor
+  | ATTR_List     of attrvalue array
+  | ATTR_Namelist of {name : string; attr: (string * attrvalue) array}
+
 (** flt, int, ... ? *)
