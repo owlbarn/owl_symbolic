0c2ebf5a64537571ef6dc12459c69e242a2be535
Liang Wang <liang.wang@nokia.com>
2019-11-09 11:35:11 +0200

    coding onnx converter ...

--- example/example_00.py
+++ example/example_00.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import onnx
+import torch
 
 if __name__ == "__main__":
+    input = torch.randn(10, 3, 224, 224)
     model = onnx.load("test.onnx")
+    onnx.checker.check_model(model)
     print(model)
