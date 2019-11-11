57a0efc38a96fe38eba097a4f49661842a852439
jzstark <rho.ajax@gmail.com>
2019-11-11 18:31:32 +0000

    update example

--- example/example_01.ml
+++ example/example_01.ml
@@ -2,7 +2,8 @@ open Owl_symbolic
 
 let _ =
   (* let x = Op.(add (flt 3.) (sin (tensor [| 3; 3 |]))) in *)
-  let x = Op.(flt 666.) in
+  (* let x = Op.(flt 666.) in *)
+  let x = Op.(add (flt 666.) (flt 1.)) in
   (* let x = Op.(tensor [|3;3|]) in *)
   let g = SymGraph.make_graph [| x |] "sym_graph" in
   let y = ONNX_Engine.of_symbolic g in
