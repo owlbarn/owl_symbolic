80361136ab708d3bd0a0beb78880fb03ac36d27d
jzstark <rho.ajax@gmail.com>
2019-11-19 19:47:25 +0000

    updating examples

--- example/example_05.py
+++ example/example_05.py
@@ -6,7 +6,7 @@ import onnxruntime as rt
 
 sess = rt.InferenceSession("test.onnx")
 input_name_y = sess.get_inputs()[0].name
-input_y = y = np.asarray(3., dtype="float32")
+input_y = np.asarray(3., dtype="float32")
 pred_onx = sess.run(None, {input_name_y: input_y})[0]
 print(pred_onx)
 
