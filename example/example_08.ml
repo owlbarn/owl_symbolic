08511478590dadccd86cafc7bd4266ca18751542
jzstark <rho.ajax@gmail.com>
2019-11-30 22:01:25 +0000

    coding...

---
+++ example/example_08.ml
@@ -0,0 +1,13 @@
+open Owl_symbolic
+open Op
+open Infix
+open Type
+
+let x = variable "x"
+let y = (int 6) / (int 4) + (int 1) + x
+let g = SymGraph.make_graph [| y |] "sym_graph"
+
+(* Target
+let s = Owl_symbolic_cas.build g
+let _ = Owl_symbolic_cas.pprint s
+*)
---
+++ src/cas/owl_symbolic_cas_tree.ml
@@ -0,0 +1,6 @@
+(*
+ * OWL - OCaml Scientific and Engineering Computing
+ * Copyright (c) 2016-2019 Liang Wang <liang.wang@cl.cam.ac.uk>
+ *)
+
+(** Convert symbol_graph into functional-rich cse_graph (or tree actually) *)
