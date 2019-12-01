95f7bf54120b1e5fc342c1ab04744d90640f2348
jzstark <rho.ajax@gmail.com>
2019-12-01 00:20:13 +0000

    coding...

--- src/cas/owl_symbolic_cas_operator.ml
+++ src/cas/owl_symbolic_cas_operator.ml
@@ -4,3 +4,19 @@
  *)
 
  (** Target: Int, Rational, Var, Add *)
+
+(*
+ module Int = struct
+
+  type t = {
+    v : int 
+  }
+
+  let create v = 
+    if v = 1 then one () else 
+    if v = 0 then zero () else 
+    if v = -1 then negzero () else 
+    int v
+  
+ end 
+ *)
--- src/cas/owl_symbolic_cas_tree.ml
+++ src/cas/owl_symbolic_cas_tree.ml
@@ -4,3 +4,9 @@
  *)
 
 (** Convert symbol_graph into functional-rich cse_graph (or tree actually) *)
+
+(* convert sym_graph to cas_graph *)
+let build _sym_graph = ()
+
+(* print expressoin on stdout *)
+let pprint _cas_graph = ()
