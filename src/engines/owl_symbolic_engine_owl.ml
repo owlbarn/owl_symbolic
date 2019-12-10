395c453fb2611d0570db72412d19fb89d65bb423
jzstark <rho.ajax@gmail.com>
2019-12-10 13:34:16 +0000

    refactoring

--- src/engines/owl_symbolic_engine_owl.ml
+++ src/engines/owl_symbolic_engine_owl.ml
@@ -16,6 +16,98 @@ module Make (G : Owl_computation_engine_sig.Flatten_Sig) = struct
 
   type t = G.graph
 
+  let _build_symbol_var op name =
+    let shape = op.shape in
+    let s =
+      match shape.(0) with
+      | Some s -> s
+      | None   -> failwith "unspecified owl shape"
+    in
+    let s = Owl_symbolic_utils.to_nchw_order s in
+    (* HACK *)
+    Owl_symbolic_operator.variable ~shape:s ~dtype:SNT_Float name
+
+
+  let _build_symbol_zeros shp name =
+    let shp = Owl_symbolic_utils.to_nchw_order shp in
+    (* HACK *)
+    let ele_num = Owl_symbolic_utils.nelt shp in
+    let flt_val = Array.make ele_num 0. in
+    let tensor = Owl_symbolic_types.make_tensor ~flt_val shp in
+    Owl_symbolic_operator.tensor ~name tensor
+
+
+  let _build_symbol_ones shp name =
+    let shp = Owl_symbolic_utils.to_nchw_order shp in
+    (* HACK *)
+    let ele_num = Owl_symbolic_utils.nelt shp in
+    let flt_val = Array.make ele_num 1. in
+    let tensor = Owl_symbolic_types.make_tensor ~flt_val shp in
+    Owl_symbolic_operator.tensor ~name tensor
+
+
+  let _build_symbol_uniform shp node =
+    let name = Owl_graph.name node in
+    let shp = Owl_symbolic_utils.hwio_to_oihw_order shp in
+    (* HACK *)
+    let inodes = Owl_graph.parents node in
+    let low =
+      G.Optimiser.Operator.Symbol.node_to_elt inodes.(0)
+      |> G.Optimiser.Operator.Symbol.elt_to_float
+    in
+    let high =
+      G.Optimiser.Operator.Symbol.node_to_elt inodes.(1)
+      |> G.Optimiser.Operator.Symbol.elt_to_float
+    in
+    Owl_symbolic_operator.random_uniform ~name ~high ~low shp
+
+
+  let _build_symbol_const node =
+    let cnode_attr = Owl_graph.attr node in
+    let name = Owl_graph.name node in
+    let shape =
+      match cnode_attr.shape.(0) with
+      | Some s -> s
+      | None   -> failwith "Const: unspecified owl shape"
+    in
+    let shape = Owl_symbolic_utils.to_nchw_order shape in
+    (* HACK *)
+    let flt_val =
+      if shape = [||]
+      then
+        [| G.Optimiser.Operator.Symbol.node_to_elt node
+           |> G.Optimiser.Operator.Symbol.elt_to_float
+        |]
+      else
+        (* TODO: G.node_to_arr node |> G.unpack_arr |> A.to_array *)
+        failwith "Convert constant Ndarray value is not supported yet."
+    in
+    (* TODO: change the dtype to float/double accoding to specific Owl ndarray type *)
+    let t = Owl_symbolic_types.make_tensor ~flt_val shape in
+    Owl_symbolic_operator.tensor ~name t
+
+
+  let _build_symbol_sum' cnode_attr name sym_inputs =
+    let shape = cnode_attr.shape in
+    let len =
+      match shape.(0) with
+      | Some s -> Array.length s
+      | None   -> failwith "Owl_engine/sum':unspecified owl shape."
+    in
+    let axes = Owl_utils_array.range 0 (len - 1) in
+    Owl_symbolic_operator.reduce_sum ~name ~keepdims:false sym_inputs.(0) axes
+
+
+  let _build_symbol_reshape shp name sym_inputs =
+    let shp = Owl_symbolic_utils.to_nchw_order shp in
+    (* HACK *)
+    let t =
+      Owl_symbolic_types.make_tensor ~dtype:SNT_Int64 ~int_val:shp [| Array.length shp |]
+    in
+    let shp_node = Owl_symbolic_operator.tensor t in
+    Owl_symbolic_operator.reshape ~name sym_inputs.(0) shp_node
+
+
   let build_symbol node symdict =
     let cnode_attr = Owl_graph.attr node in
     let name = Owl_graph.name node in
@@ -28,64 +120,12 @@ module Make (G : Owl_computation_engine_sig.Flatten_Sig) = struct
         (Owl_graph.parents node)
     in
     match cnode_attr.op with
-    | Var ->
-      let shape = cnode_attr.shape in
-      let s =
-        match shape.(0) with
-        | Some s -> s
-        | None   -> failwith "unspecified owl shape"
-      in
-      let s = Owl_symbolic_utils.to_nchw_order s in
-      (* HACK *)
-      Owl_symbolic_operator.variable ~shape:s ~dtype:SNT_Float name
-    | Zeros shp ->
-      let shp = Owl_symbolic_utils.to_nchw_order shp in
-      (* HACK *)
-      let ele_num = Owl_symbolic_utils.nelt shp in
-      let flt_val = Array.make ele_num 0. in
-      let tensor = Owl_symbolic_types.make_tensor ~flt_val shp in
-      Owl_symbolic_operator.tensor ~name tensor
-    | Ones shp ->
-      let shp = Owl_symbolic_utils.to_nchw_order shp in
-      (* HACK *)
-      let ele_num = Owl_symbolic_utils.nelt shp in
-      let flt_val = Array.make ele_num 1. in
-      let tensor = Owl_symbolic_types.make_tensor ~flt_val shp in
-      Owl_symbolic_operator.tensor ~name tensor
-    | Uniform shp ->
-      let shp = Owl_symbolic_utils.hwio_to_oihw_order shp in
-      (* HACK *)
-      let inodes = Owl_graph.parents node in
-      let low =
-        G.Optimiser.Operator.Symbol.node_to_elt inodes.(0)
-        |> G.Optimiser.Operator.Symbol.elt_to_float
-      in
-      let high =
-        G.Optimiser.Operator.Symbol.node_to_elt inodes.(1)
-        |> G.Optimiser.Operator.Symbol.elt_to_float
-      in
-      Owl_symbolic_operator.random_uniform ~name ~high ~low shp
-    | Const ->
-      let shape =
-        match cnode_attr.shape.(0) with
-        | Some s -> s
-        | None   -> failwith "Const: unspecified owl shape"
-      in
-      let shape = Owl_symbolic_utils.to_nchw_order shape in
-      (* HACK *)
-      let flt_val =
-        if shape = [||]
-        then
-          [| G.Optimiser.Operator.Symbol.node_to_elt node
-             |> G.Optimiser.Operator.Symbol.elt_to_float
-          |]
-        else
-          (* TODO: G.node_to_arr node |> G.unpack_arr |> A.to_array *)
-          failwith "Convert constant Ndarray value is not supported yet."
-      in
-      (* TODO: change the dtype to float/double accoding to specific Owl ndarray type *)
-      let t = Owl_symbolic_types.make_tensor ~flt_val shape in
-      Owl_symbolic_operator.tensor ~name t
+    (* TODO: unify interfaces *)
+    | Var -> _build_symbol_var cnode_attr name
+    | Zeros shp -> _build_symbol_zeros shp name
+    | Ones shp -> _build_symbol_ones shp name
+    | Uniform shp -> _build_symbol_uniform shp node
+    | Const -> _build_symbol_const node
     | Sin -> Owl_symbolic_operator.sin ~name sym_inputs.(0)
     | Cos -> Owl_symbolic_operator.cos ~name sym_inputs.(0)
     | Sqrt -> Owl_symbolic_operator.sqrt ~name sym_inputs.(0)
@@ -113,27 +153,9 @@ module Make (G : Owl_computation_engine_sig.Flatten_Sig) = struct
     | Dot (_, _, _, _) -> Owl_symbolic_operator.matmul ~name sym_inputs.(0) sym_inputs.(1)
     | SumReduce a -> Owl_symbolic_operator.reduce_sum ~name sym_inputs.(0) a
     | Sum a -> Owl_symbolic_operator.reduce_sum ~name sym_inputs.(0) [| a |]
-    | Sum' ->
-      let shape = cnode_attr.shape in
-      let len =
-        match shape.(0) with
-        | Some s -> Array.length s
-        | None   -> failwith "Owl_engine/sum':unspecified owl shape."
-      in
-      let axes = Owl_utils_array.range 0 (len - 1) in
-      Owl_symbolic_operator.reduce_sum ~name ~keepdims:false sym_inputs.(0) axes
+    | Sum' -> _build_symbol_sum' cnode_attr name sym_inputs
     | Max a -> Owl_symbolic_operator.reduce_max ~name sym_inputs.(0) [| a |]
-    | Reshape shp ->
-      let shp = Owl_symbolic_utils.to_nchw_order shp in
-      (* HACK *)
-      let t =
-        Owl_symbolic_types.make_tensor
-          ~dtype:SNT_Int64
-          ~int_val:shp
-          [| Array.length shp |]
-      in
-      let shp_node = Owl_symbolic_operator.tensor t in
-      Owl_symbolic_operator.reshape ~name sym_inputs.(0) shp_node
+    | Reshape shp -> _build_symbol_reshape shp name sym_inputs
     | Conv2d (padding, stride) ->
       let pad = if padding = SAME then "SAME" else "VALID" in
       Owl_symbolic_operator.conv ~name sym_inputs.(0) sym_inputs.(1) pad stride [| 1; 1 |]
