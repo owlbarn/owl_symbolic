3753e88aa9dca6866d74c98d4cdf634099570136
jzstark <rho.ajax@gmail.com>
2019-12-14 15:43:30 +0000

    add test example: maxpool

--- example/example_09.ml
+++ example/example_09.ml
@@ -1,7 +1,8 @@
 open Owl_symbolic
 open Op
+open Type
 
-(*
+(* 
 let test_gemm () =
   let a = variable ~shape:[| 3; 4 |] "a" in
   let b = variable ~shape:[| 4; 5 |] "b" in
@@ -13,12 +14,21 @@ let test_gemm () =
   ONNX_Engine.save y "test.onnx"
 *)
 
+(*
 let test_split () =
   let a = variable ~shape:[| 3; 5 |] "a" in
   let x = split ~axis:1 a [| 2; 3 |] in
   let g = SymGraph.make_graph x "sym_graph" in
   let y = ONNX_Engine.of_symbolic g in
   ONNX_Engine.save y "test.onnx"
+*)
+
+let test_maxpool () =
+  let a = variable ~shape:[| 2; 6; 6; 2 |] "a" in
+  let x, y = maxpool ~strides:[| 2; 2 |] ~padding:VALID a [| 2; 2 |] in
+  let g = SymGraph.make_graph [| x; y |] "sym_graph" in
+  let z = ONNX_Engine.of_symbolic g in
+  ONNX_Engine.save z "test.onnx"
 
 
-let _ = test_split ()
+let _ = test_maxpool ()
--- example/example_09.py
+++ example/example_09.py
@@ -21,6 +21,15 @@ flag01 = np.array_equal(pred_onnx,
     [5., 5., 5., 5., 5.]]))
 """
 
+""" 2. Split  
+sess = rt.InferenceSession("test.onnx")
+a = sess.get_inputs()[0].name
+a_input = np.ones(sess.get_inputs()[0].shape).astype(np.float32)
+pred_onnx = sess.run(None, {a: a_input})
+print(pred_onnx[0])
+print(pred_onnx[1])
+"""
+
 sess = rt.InferenceSession("test.onnx")
 a = sess.get_inputs()[0].name
 a_input = np.ones(sess.get_inputs()[0].shape).astype(np.float32)
